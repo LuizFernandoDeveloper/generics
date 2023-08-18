dc83f62311125e0ac6f9d21649650f06e8b766e9
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-08-18 15:45:29 -0300

    metodo generico

--- src/App.java
+++ src/App.java
@@ -1,20 +1,39 @@
-import java.util.Scanner;
 
-import services.PrintService;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Product;
+import services.CalculationService;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        PrintService<Integer> ps = new PrintService<Integer>();
-        System.out.print("How many  values? ");
-        int n  = sc.nextInt();
 
-        for (int i = 0; i < n ; i++){
-            int values = sc.nextInt();
-            ps.addValue(values);
-        }
-        ps.print();
-        System.out.println("Frist: " + ps.frist());
-        sc.close();
-    }
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		
+		List<Product> list = new ArrayList<>();
+
+		String path = "C:\\temp\\in.txt";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+				line = br.readLine();
+			}
+			
+			Product x = CalculationService.max(list);
+			System.out.println("Most expensive:");
+			System.out.println(x);
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} 
+	}
 }
